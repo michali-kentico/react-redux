21a07fdc46b40b1fb8594b0cd46c205609953fa5
Michal Ingr <michali@kentico.com>
2017-07-14 12:08:41 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+// Importování stylů
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
